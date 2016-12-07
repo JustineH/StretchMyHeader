f39d6dc48aabe2c1331a3932b8e497b065025f61
Justine <justine.m.h@gmail.com>
2016-12-06 16:15:09 -0800

    Set up table view, NewsItem struct

---
+++ StretchMyHeader/StretchMyHeader/NewsItemCell.swift
@@ -0,0 +1,42 @@
+//
+//  NewsItemCell.swift
+//  StretchMyHeader
+//
+//  Created by Justine Herman on 12/6/16.
+//  Copyright © 2016 Justine Herman. All rights reserved.
+//
+
+import UIKit
+
+class NewsItemCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    var newsItem: NewsItem? {
+        didSet {
+            if let item = newsItem {
+                categoryLabel.text = item.category.toString()
+                categoryLabel.textColor = item.category.toColor()
+                summaryLabel.text = item.summary
+            }
+            else {
+                categoryLabel.text = nil
+                summaryLabel.text = nil
+            }
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ StretchMyHeader/StretchMyHeader/UITableViewController.swift
@@ -0,0 +1,86 @@
+//
+//  UITableViewController.swift
+//  StretchMyHeader
+//
+//  Created by Justine Herman on 12/6/16.
+//  Copyright © 2016 Justine Herman. All rights reserved.
+//
+
+import UIKit
+
+class UITableViewController: UIViewController {
+    
+    let cellIdentifier = "kTableViewCell"
+    
+    let items = [
+        NewsItem(category: .World, summary: "Climate change protests, divestments meet fossil fuels realities"),
+        NewsItem(category: .Europe, summary: "Scotland's 'Yes' leader says independence vote is 'once in a lifetime'"),
+        NewsItem(category: .MiddleEast, summary: "Airstrikes boost Islamic State, FBI director warns more hostages possible"),
+        NewsItem(category: .Africa, summary: "Nigeria says 70 dead in building collapse; questions S. Africa victim claim"),
+        NewsItem(category: .AsiaPacific, summary: "Despite UN ruling, Japan seeks backing for whale hunting"),
+        NewsItem(category: .Americas, summary: "Officials: FBI is tracking 100 Americans who fought alongside IS in Syria"),
+        NewsItem(category: .World, summary: "South Africa in $40 billion deal for Russian nuclear reactors"),
+        NewsItem(category: .Europe, summary: "'One million babies' created by EU student exchanges"),
+    ]
+
+    // MARK: Properties
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func preferStatusBarHidden() -> Bool
+    {
+        return true
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return UITableViewAutomaticDimension
+    }
+
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! NewsItemCell
+        
+        
+
+        cell.newsItem = items[indexPath.row]
+
+        return UITableViewCell()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
